66c39a037c96270aa1b54785dcdcb8a05a35e466
je-r <5433476+je-r@users.noreply.github.com>
2021-02-03 21:11:58 +0100

    dev

--- jerVoting/jerVotingWeb/ServiceEventSource.cs
+++ jerVoting/jerVotingWeb/ServiceEventSource.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.ServiceFabric.Services.Runtime;
-
+//
 namespace jerVotingWeb
 {
     [EventSource(Name = "MyCompany-jerVoting-jerVotingWeb")]
